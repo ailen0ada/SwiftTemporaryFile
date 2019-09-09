3380127f1cbef874c99ad056b995c0e66e640a96
YOCKOW <YOCKOW@users.noreply.github.com>
2019-09-09 14:50:00 +0900

    Update dependency: ySwiftExtensions

--- Package.swift
+++ Package.swift
@@ -11,12 +11,12 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/YOCKOW/SwiftExtensions.git", from: "0.3.1"),
+    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", from: "0.4.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    .target(name: "TemporaryFile", dependencies: ["FileHandle__", "SwiftYOCKOWExtensions"]),
+    .target(name: "TemporaryFile", dependencies: ["FileHandle__", "ySwiftExtensions"]),
     .target(name: "FileHandle__", dependencies: []),
     .testTarget(name: "TemporaryFileTests", dependencies: ["TemporaryFile"]),
   ],
