7d954f4cc960e07ef672d74ad5b3edfa766a8e6c
YOCKOW <YOCKOW@users.noreply.github.com>
2020-01-27 14:26:39 +0900

    Update ySwiftExtensions' version.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", from: "0.7.3"),
+    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", from: "0.7.4"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
