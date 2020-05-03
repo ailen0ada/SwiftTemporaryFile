b2a1cd386d84ce22ff2da2fc0387dd892f7b2735
YOCKOW <YOCKOW@users.noreply.github.com>
2020-05-03 13:07:23 +0900

    Drop support for older OS and older Swift (due to ySwiftExtensions).

--- Package.swift
+++ Package.swift
@@ -1,17 +1,23 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "TemporaryFile",
+  platforms: [
+    .macOS(.v10_15),
+    .iOS(.v13),
+    .watchOS(.v6),
+    .tvOS(.v13),
+  ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(name: "SwiftTemporaryFile", type:.dynamic, targets: ["TemporaryFile"]),
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", "0.10.0"..<"2.0.0"),
+    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", from: "1.1.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -19,7 +25,7 @@ let package = Package(
     .target(name: "TemporaryFile", dependencies: ["ySwiftExtensions"]),
     .testTarget(name: "TemporaryFileTests", dependencies: ["TemporaryFile", "ySwiftExtensions"]),
   ],
-  swiftLanguageVersions:[.v4, .v4_2, .v5]
+  swiftLanguageVersions:[.v5]
 )
 
 import Foundation
