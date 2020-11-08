e7eb2b805895eb92326dcb3668057fb1daf8608b
YOCKOW <YOCKOW@users.noreply.github.com>
2020-11-08 16:40:48 +0900

    Update dependencies.

--- Package.swift
+++ Package.swift
@@ -17,12 +17,19 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", from: "1.2.0"),
+    .package(url: "https://github.com/YOCKOW/SwiftRanges.git", from: "3.1.1"),
+    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", from: "1.6.1"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    .target(name: "TemporaryFile", dependencies: ["ySwiftExtensions"]),
+    .target(
+      name: "TemporaryFile",
+      dependencies: [
+        "SwiftRanges",
+        "ySwiftExtensions",
+      ]
+    ),
     .testTarget(name: "TemporaryFileTests", dependencies: ["TemporaryFile", "ySwiftExtensions"]),
   ],
   swiftLanguageVersions:[.v5]
--- Sources/TemporaryFile/InMemoryFile.swift
+++ Sources/TemporaryFile/InMemoryFile.swift
@@ -6,6 +6,7 @@
  ************************************************************************************************ */
 
 import Foundation
+@_implementationOnly import Ranges
 import yExtensions
 import yProtocols
 
